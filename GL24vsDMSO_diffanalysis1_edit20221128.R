2cccdffbeacbccf7e0f9c7e7728aecced292c975
chenyc0901 <chenyc0901@gmail.com>
2022-12-23 11:23:29 +0800

    20221223

--- GL24vsDMSO_diffanalysis1_edit20221128.R
+++ GL24vsDMSO_diffanalysis1_edit20221128.R
@@ -91,44 +91,41 @@ mergedata <- left_join(dgetable_Hs578T, G_list, by = c("gene_id"="ensembl_gene_i
 ##### To analysize the result ######
 #To draw volcanoplot 
 proteincoding_data <- mergedata %>% filter(mergedata$gene_biotype=="protein_coding")
-#overlap(112 DE gene overlap, up=42, down=70 )
-overlap_up <- proteincoding_data$logFC_MDAMB157 > 2 & 
-  proteincoding_data$logFC_MDAMB231 > 2 & 
-  proteincoding_data$FDR_MDAMB157 < 10e-5 & 
-  proteincoding_data$FDR_MDAMB231 < 10e-5 
-overlap_down <-  proteincoding_data$logFC_MDAMB157 < -2 & 
-  proteincoding_data$logFC_MDAMB231 < -2 & 
-  proteincoding_data$FDR_MDAMB157 < 10e-5 & 
-  proteincoding_data$FDR_MDAMB231 < 10e-5 
-#add color panel
-proteincoding_data <- proteincoding_data %>% mutate(color=ifelse(overlap_up,'red',ifelse(overlap_down,'blue','grey')))
+
+proteincoding_data %>% filter(abs(logFC_Hs578T)>1 & FDR_Hs578T<0.01) %>% glimpse()
 
 draw_volcanoplot <- function(cell){
                     logFC <- paste0("logFC_",cell)
                     FDR <- paste0("FDR_",cell)
                     output <- paste0(cell,".pdf")
+                    Up_regulated <- proteincoding_data[logFC] > 1 & proteincoding_data[FDR] < 1e-2 
+                    Down_regulated <- proteincoding_data[logFC] < -1 & proteincoding_data[FDR] < 1e-2 
+                    proteincoding_data <- proteincoding_data %>% mutate(color = ifelse(Up_regulated, 'red', ifelse(Down_regulated, 'blue', 'grey')))
                     keyvals.color <- proteincoding_data$color
-                    names(keyvals.color)[keyvals.color == 'red'] <- 'overlap_up'
-                    names(keyvals.color)[keyvals.color == 'blue'] <- 'overlap_down'
-                    names(keyvals.color)[keyvals.color == 'grey'] <- 'non-overlap'
+                    names(keyvals.color)[keyvals.color == 'red'] <- 'Up-regulated'
+                    names(keyvals.color)[keyvals.color == 'blue'] <- 'Down-regulated'
+                    names(keyvals.color)[keyvals.color == 'grey'] <- 'Not-significant'
                     volcanoplot <- EnhancedVolcano(proteincoding_data,
                                    lab=NA,
-                                    #lab=proteincoding_data$hgnc_symbol,
-                                    #selectLab=proteincoding_data[which(filtercondition),]$hgnc_symbol,
+                                   title=cell,
                                     xlab = bquote(~Log[2]~ 'fold change'),
+                                    ylab = bquote(~-Log[10]~ '(adjusted p value)'),
                                     x=logFC,
                                     y=FDR,
                                     colCustom=keyvals.color,
-                                    legendPosition = 'top',
+                                    legendPosition = 'right',
                                     legendLabSize = 10,
-                                    FCcutoff = 2,
-                                    pCutoff= 10e-5,
-                                    colAlpha = 0.3,
+                                    FCcutoff = 1,
+                                    pCutoff= 1e-2,
+                                    colAlpha = 1,
                                     drawConnectors = TRUE,
                                     widthConnectors = 0.75,
                                     maxoverlapsConnectors = Inf,
                                     colConnectors = 'black',
-                                    max.overlaps = Inf) 
+                                    #col=c("blue", "grey", "grey", "red"),
+                                    max.overlaps = Inf) +
+                      scale_x_continuous(breaks = seq(-10,10,5), limits = c(-10,10))+
+                      scale_y_continuous(breaks = seq(0,30,5), limits = c(0,30))
                    ggsave(volcanoplot, file=output, width=17 ,height=17, units="cm", device='pdf')
 }
 draw_volcanoplot("Hs578T")
@@ -137,25 +134,25 @@ draw_volcanoplot("MDAMB157")
 
 #To draw heatmap by logCPM
 logCPM  <- cpm(y, log=TRUE) %>% 
-  as.data.frame() %>% 
-  rownames_to_column("gene_id") 
+           as.data.frame() %>% 
+           rownames_to_column("gene_id") 
 logCPM <- left_join(logCPM, G_list, by = c("gene_id"="ensembl_gene_id")) %>% 
-  filter(gene_biotype=="protein_coding") %>% 
-  distinct(gene_id,.keep_all=TRUE)
-rownames(logCPM) <- logCPM %>%
-  mutate(name = paste(.$gene_id, .$hgnc_symbol, sep=";")) %>% 
-  .$name
+          filter(gene_biotype=="protein_coding") %>% 
+          distinct(gene_id,.keep_all=TRUE)
+          rownames(logCPM) <- logCPM %>%
+          mutate(name = paste(.$gene_id, .$hgnc_symbol, sep=";")) %>% 
+          .$name
 
 logCPM_mt <- logCPM %>%
-  dplyr::select(-c(gene_id,entrezgene_id,gene_biotype,hgnc_symbol)) %>% 
-  as.matrix()
+             dplyr::select(-c(gene_id,entrezgene_id,gene_biotype,hgnc_symbol)) %>% 
+             as.matrix()
 
-#filter condition(MDAMB231,MDAMB157): FDR <10e-5, logFC > 3
+#filter condition(MDAMB231,MDAMB157): FDR <0.01, abs(logFC) > 1, n=1047
 gene.list <- proteincoding_data %>% 
-                   dplyr::filter(FDR_MDAMB231 < 10e-5 & 
-                                 FDR_MDAMB157 < 10e-5 & 
-                                 abs(logFC_MDAMB231)> 3 & 
-                                 abs(logFC_MDAMB157) > 3 ) %>% 
+                   dplyr::filter(FDR_MDAMB231 < 0.01 & 
+                                 FDR_MDAMB157 < 0.01 & 
+                                 abs(logFC_MDAMB231)> 1 & 
+                                 abs(logFC_MDAMB157) > 1 ) %>% 
                    mutate(name=paste(.$gene_id, .$hgnc_symbol, sep=";")) %>%
                    .$name %>% 
                    as.character()
@@ -166,10 +163,11 @@ logCPM_mt%>% head()
 logCPM_mt_transpose <- t(scale(t(logCPM_mt)))
 col.pan <- colorpanel(100, "blue", "white", "red")
 
-pdf("heatmap.pdf")
+pdf("heatmap_1047.pdf")
 ColSideColors <- cbind(Treatment=rep(c("orange", "green"), each=5, times=3),Cell_type=c(rep("brown1",10),rep("mediumpurple2",20)))
 heatmap3(logCPM_mt_transpose, 
           col=col.pan, 
+          labRow=FALSE,
           Rowv=TRUE,
           Colv=TRUE,
           scale="none",
@@ -177,4 +175,38 @@ heatmap3(logCPM_mt_transpose,
           margin=c(10,9),
           cexRow=0.7,
           cexCol=0.8)
+dev.off()
+
+
+
+#filter condition(MDAMB231,MDAMB157): FDR <0.01, abs(logFC) > 1,same direction, n=959
+
+gene.list <- proteincoding_data %>% 
+             dplyr::filter(FDR_MDAMB231 < 0.01 & 
+             FDR_MDAMB157 < 0.01 & 
+             abs(logFC_MDAMB231)> 1 & 
+             abs(logFC_MDAMB157) > 1 &
+             logFC_MDAMB231*logFC_MDAMB157>0) %>% 
+             mutate(name=paste(.$gene_id, .$hgnc_symbol, sep=";")) %>%
+             .$name %>% 
+             as.character()
+
+gene.list %>% str()
+logCPM_mt <- logCPM_mt[gene.list,]
+logCPM_mt%>% head()
+logCPM_mt_transpose <- t(scale(t(logCPM_mt)))
+col.pan <- colorpanel(100, "blue", "white", "red")
+
+pdf("heatmap_same_direction_959.pdf")
+ColSideColors <- cbind(Treatment=rep(c("orange", "green"), each=5, times=3),Cell_type=c(rep("brown1",10),rep("mediumpurple2",20)))
+heatmap3(logCPM_mt_transpose, 
+         col=col.pan, 
+         labRow=FALSE,
+         Rowv=TRUE,
+         Colv=TRUE,
+         scale="none",
+         ColSideColors = ColSideColors, 
+         margin=c(10,9),
+         cexRow=0.7,
+         cexCol=0.8)
 dev.off()
